9a3e75295705de88aa289feb308e15e88c6dd758
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2022-08-23 21:51:01 +0530

    :octocat: mototabg62: Initial device makefiles
    Signed-off-by: Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_mototabg62.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_mototabg62-user \
+    lineage_mototabg62-userdebug \
+    lineage_mototabg62-eng
